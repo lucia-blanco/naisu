6d2c6e5087f45b2edd998f98d3348ede28328d8a
unknown <lublasal@gmail.com>
2018-04-17 14:23:38 +0200

    Post

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent implements OnInit {
+export class PostComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor( private route: ActivatedRoute,
+                public _ps: PostService) {
+                  route.params.subscribe( parametros => {
+                 _ps.load_post( parametros['id'])
+                 .subscribe ( res => {
+                    this.cod = parametros['id'];
+                    this.post = res.json();
+                 });
+            });
+          }
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class PostService {
 
-  constructor() { }
+  posts: any[] = [];
+  loading_posts = true;
+  loading_post = true;
 
+  constructor( private http: Http) {
+    this.load_posts();
+  }
+
+  public load_post( cod: string) {
+    setTimeout(() => {
+      this.loading_post = true;
+      return this.http.get(`https://naisu-6fa2c.firebaseio.com/post/${ cod }.json`);
+    }, 800);
+  }
+
+  public load_posts() {
+
+    this.loading_posts = true;
+
+    const promesa = new Promise(( resolve, reject ) => {
+
+      this.http.get('https://naisu-6fa2c.firebaseio.com/home.json')
+        .subscribe( res => {
+          // console.log( res.json());
+
+          setTimeout(() => {
+            this.loading_posts = false;
+            this.posts = res.json();
+            resolve();
+          }, 800);
+        });
+    });
+
+    return promesa;
+
+  }
 }
