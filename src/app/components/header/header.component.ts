3e7c14ea4c824374fa115f40742c373fd7a5c937
unknown <lublasal@gmail.com>
2018-04-17 13:47:55 +0200

    Header with Firebase

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { InfoService } from '../../services/info.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(public _is: InfoService) { }
 
 }
